1d2f7baa19dcb7d264cdc13673c71a6201ea5546
HOWOOK <37730885+HOWOOK@users.noreply.github.com>
2018-08-16 12:28:51 +0900

    design

---
+++ CacheMission/app/src/main/java/com/example/hwshin/cachemission/Activity/SettingActivity.java
@@ -0,0 +1,19 @@
+package com.example.hwshin.cachemission.Activity;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hwshin.cachemission.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+
+    }
+
+}
