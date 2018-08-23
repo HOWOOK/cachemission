7a81fa4893640cfd42877f798126a5d2579b9934
chohayoung54 <40687275+chohayoung54@users.noreply.github.com>
2018-08-23 19:51:01 +0900

    0823
    0823

--- CacheMission/app/src/main/java/com/example/hwshin/cachemission/DataStructure/Controller/Controller_Voice.java
+++ CacheMission/app/src/main/java/com/example/hwshin/cachemission/DataStructure/Controller/Controller_Voice.java
@@ -3,153 +3,106 @@ package com.example.hwshin.cachemission.DataStructure.Controller;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
-import android.support.v4.content.ContextCompat;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.hwshin.cachemission.R;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class Controller_Voice extends Controller {
-    boolean isRecording=false;
-    boolean isPlaying = false;
-    boolean isPausing = false;
-    MediaRecorder recorder=new MediaRecorder();
-    MediaPlayer mPlayer = new MediaPlayer();
-    String oldpath="init";
-    String path;
-    int serverResponseCode = 0;
+
     public Controller_Voice() {
         controllerID = R.layout.controller_voice;
     }
+    private final int mBufferSize = 1024;
+    private final int mBytesPerElement = 2;
+
+// 설정할 수 있는 sampleRate, AudioFormat, channelConfig 값들을 정의
+
+    private final int[] mSampleRates = new int[] {44100, 22050, 11025, 8000};
+    private final short[] mAudioFormats = new short[] {AudioFormat.ENCODING_PCM_16BIT, AudioFormat.ENCODING_PCM_8BIT};
+    private final short[] mChannelConfigs = new short[] {AudioFormat.CHANNEL_IN_STEREO, AudioFormat.CHANNEL_IN_MONO};
+
+// 위의 값들 중 실제 녹음 및 재생 시 선택된 설정값들을 저장
+
+    private int mSampleRate;
+    private short mAudioFormat;
+    private short mChannelConfig;
+
+    private AudioRecord mRecorder = null;
+    private Thread mRecordingThread = null;
+    private Button mRecordBtn, mPlayBtn;
+    private boolean mIsRecording = false;           // 녹음 중인지에 대한 상태값
+    private String mPath = "";                      // 녹음한 파일을 저장할 경로
+
+    private boolean isPlaying = false;
+    MediaPlayer mPlayer = new MediaPlayer();
+private String oldpath="init";
+    int serverResponseCode = 0;
 
 
-    public void startRec(){
-
-        try {
-
-            File file= Environment.getExternalStorageDirectory();
-//갤럭시 S4기준으로 /storage/emulated/0/의 경로를 갖고 시작한다.
-            if(oldpath!="init"){
-                Uri i= Uri.parse(oldpath);
-                File f=new File(i.getPath());
-                f.delete();
-            }
-            path=file.getAbsolutePath()+"/"+"recoder_"+System.currentTimeMillis()+".wav";
-            oldpath=path;
-            recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            recorder.setAudioSamplingRate(44100);
-            //recorder.setAudioEncodingBitRate(1);
-//첫번째로 어떤 것으로 녹음할것인가를 설정한다. 마이크로 녹음을 할것이기에 MIC로 설정한다.
-            recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-//이것은 파일타입을 설정한다. 녹음파일의경우 3gp로해야 용량도 작고 효율적인 녹음기를 개발할 수있다.
-            recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-//이것은 코덱을 설정하는 것이라고 생각하면된다.
-            recorder.setOutputFile(path);
-//저장될 파일을 저장한뒤
-            recorder.prepare();
-            recorder.start();
-//시작하면된다.
-
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void stopRec(){
-        try{
-            Log.d("STOPREC","STOPPED");
-            recorder.stop();
-        }catch(RuntimeException e){
-            e.printStackTrace();
-        }
-//멈추는 것이다.
-
-    }
     @Override
     public void setLayout(final String id, View view, Context c, Intent in, String buttons) {
-
-
-
-        final Context c_ = c;
-        ConstraintLayout templayout = (ConstraintLayout) view;
-        Button post=templayout.findViewById(R.id.post);
-        final ImageView recb =templayout.findViewById(R.id.recb);
-        final ImageView mBtPlay = templayout.findViewById(R.id.stream);
-
-
-
-        recb.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(isRecording==false){
-                    startRec();
-                    //recorder.start();
-                    isRecording=true;
-                    recb.setImageDrawable(ContextCompat.getDrawable(c_, R.drawable.recordstopbtn));
-                    //recb.setText("녹음그만");
-                }
-                else{
-                    stopRec();
-                    isRecording=false;
-                    recb.setImageDrawable(ContextCompat.getDrawable(c_, R.drawable.recordstartbtn));
-                    //recb.setText("다시녹음하기");
-                    }
-
-            }
-        });
-
-        mBtPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isPlaying == false) {
-                    try {
-                        mPlayer.setDataSource(path);
-                        mPlayer.prepare();
-                    }catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                    mPlayer.start();
-
-                    isPlaying = true;
-                    mBtPlay.setImageDrawable(ContextCompat.getDrawable(c_, R.drawable.recordpausebtn));
-                    //mBtPlay.setText("듣기중지");
-                }
-                else {
-                    mPlayer.reset();
-
-                    isPlaying = false;
-                    mBtPlay.setImageDrawable(ContextCompat.getDrawable(c_, R.drawable.recordplaybtn));
-                    //mBtPlay.setText("들어보기");
-                }
-            }
-        });
-
+        ConstraintLayout temp=(ConstraintLayout)view;
+        mRecordBtn = (Button)temp.findViewById(R.id.recb);
+        mPlayBtn = (Button)temp.findViewById(R.id.stream);
+        mRecordBtn.setOnClickListener(btnClick);
+        mPlayBtn.setOnClickListener(btnClick);
+        Button post=temp.findViewById(R.id.post);
 
         post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                File f1 = new File(mPath+".pcm"); // The location of your PCM file
+                File f2 = new File(mPath+".wav"); // The location where you want your WAV file
+                try {
+                    rawToWave(f1, f2);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 new Thread(){
                     public void run() {
-                        uploadFile(path,id);
+                        uploadFile(mPath+".wav",id);
                         Log.d("serverres",String.valueOf(serverResponseCode));
+                        Log.d("serverres",mPath+".wav");
+                        Uri i= Uri.parse(mPath+".pcm");
+                        File f=new File(i.getPath());
+                        f.delete();
+                        Uri i2= Uri.parse(mPath+".wav");
+                        File f2=new File(i2.getPath());
+                        f2.delete();
                         if(serverResponseCode==200){
 
                             parentActivity.startActivity(parentIntent);
@@ -166,21 +119,315 @@ public class Controller_Voice extends Controller {
 
             }
         });
+    }
 
 
-        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+// 녹음을 수행할 Thread를 생성하여 녹음을 수행하는 함수
+
+    private void startRecording() {
+
+        mRecorder = findAudioRecord();
+        mRecorder.startRecording();
+        mIsRecording = true;
+        mRecordingThread = new Thread(new Runnable() {
+
+
 
             @Override
-            public void onCompletion(MediaPlayer mp) {
-                mPlayer.reset();
-                isPlaying = false;
-                mBtPlay.setImageDrawable(ContextCompat.getDrawable(c_, R.drawable.recordplaybtn));
-                //mBtPlay.setText("들어보기");
+
+            public void run() {
+                writeAudioDataToFile();
             }
-        });
+
+        }, "AudioRecorder Thread");
+        mRecordingThread.start();
+    }
 
 
 
+// 녹음을 하기 위한 sampleRate, audioFormat, channelConfig 값들을 설정
+
+
+    private AudioRecord findAudioRecord() {
+        for (int rate : mSampleRates) {
+            for (short format : mAudioFormats) {
+                for (short channel : mChannelConfigs) {
+                    try {
+
+                        int bufferSize = AudioRecord.getMinBufferSize(rate, channel, format);
+
+                        if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
+
+                            mSampleRate = rate;
+                            mAudioFormat = format;
+                            mChannelConfig = channel;
+
+                            AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, mSampleRate, mChannelConfig, mAudioFormat, bufferSize);
+
+                            if (recorder.getState() == AudioRecord.STATE_INITIALIZED) {
+
+                                return recorder;    // 적당한 설정값들로 생성된 Recorder 반환
+
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        return null;                     // 적당한 설정값들을 찾지 못한 경우 Recorder를 찾지 못하고 null 반환
+
+    }
+
+
+
+// 실제 녹음한 data를 file에 쓰는 함수
+
+    private void writeAudioDataToFile() {
+
+        String sd = Environment.getExternalStorageDirectory().getAbsolutePath();
+
+        mPath = sd + "/voice_"+System.currentTimeMillis();
+        if(oldpath!="init"){
+            Uri i= Uri.parse(oldpath);
+            File f=new File(i.getPath());
+            f.delete();
+        }
+        oldpath=mPath+".pcm";
+
+        short sData[] = new short[mBufferSize];
+        FileOutputStream fos = null;
+
+
+
+        try {
+
+            fos = new FileOutputStream(mPath+".pcm");
+
+            while (mIsRecording) {
+
+                mRecorder.read(sData, 0, mBufferSize);
+                byte bData[] = short2byte(sData);
+                fos.write(bData, 0, mBufferSize * mBytesPerElement);
+
+            }
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+// short array형태의 data를 byte array형태로 변환하여 반환하는 함수
+
+
+    private byte[] short2byte(short[] sData) {
+
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+
+        for (int i = 0; i < shortArrsize; i++) {
+
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+
+        }
+        return bytes;
+
+    }
+
+
+
+// 녹음을 중지하는 함수
+
+    private void stopRecording() {
+
+        if (mRecorder != null) {
+            mIsRecording = false;
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+            mRecordingThread = null;
+
+        }
+    }
+
+
+
+// 녹음할 때 설정했던 값과 동일한 설정값들로 해당 파일을 재생하는 함수
+
+
+    private void playWaveFile() {
+
+        int minBufferSize = AudioTrack.getMinBufferSize(mSampleRate, mChannelConfig, mAudioFormat);
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, mSampleRate, mChannelConfig, mAudioFormat, minBufferSize, AudioTrack.MODE_STREAM);
+        int count = 0;
+        byte[] data = new byte[mBufferSize];
+
+        try {
+
+            FileInputStream fis = new FileInputStream(mPath+".pcm");
+            DataInputStream dis = new DataInputStream(fis);
+            audioTrack.play();
+
+            while ((count = dis.read(data, 0, mBufferSize)) > -1) {
+
+                audioTrack.write(data, 0, count);
+
+            }
+
+            audioTrack.stop();
+            audioTrack.release();
+            dis.close();
+            fis.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+
+    private View.OnClickListener btnClick = new View.OnClickListener() {
+
+
+        @Override
+
+        public void onClick(View v) {
+            switch (v.getId()) {
+
+// 녹음 버튼일 경우 녹음 중이지 않을 때는 녹음 시작, 녹음 중일 때는 녹음 중지로 텍스트 변경
+                case R.id.recb:
+
+                    if (mIsRecording == false) {
+
+                        startRecording();
+                        mIsRecording = true;
+                        mRecordBtn.setText("Stop Recording");
+
+                    } else {
+
+                        stopRecording();
+                        mIsRecording = false;
+                        mRecordBtn.setText("Start Recording");
+
+                    }
+                    break;
+
+                case R.id.stream:
+
+// 녹음 파일이 없는 상태에서 재생 버튼 클릭 시, 우선 녹음부터 하도록 Toast 표시
+
+                    if ((mPath+".pcm").length() == 0 || mIsRecording) {
+                        Toast.makeText(parentActivity, "Please record, first.", Toast.LENGTH_SHORT).show();
+                        return;
+
+                    }
+// 녹음된 파일이 있는 경우 해당 파일 재생
+                    playWaveFile();
+                    break;
+            }
+        }
+
+    };
+
+    private void rawToWave(final File rawFile, final File waveFile) throws IOException {
+
+        byte[] rawData = new byte[(int) rawFile.length()];
+        DataInputStream input = null;
+        try {
+            input = new DataInputStream(new FileInputStream(rawFile));
+            input.read(rawData);
+        } finally {
+            if (input != null) {
+                input.close();
+            }
+        }
+
+        DataOutputStream output = null;
+        try {
+            output = new DataOutputStream(new FileOutputStream(waveFile));
+            // WAVE header
+            // see http://ccrma.stanford.edu/courses/422/projects/WaveFormat/
+            writeString(output, "RIFF"); // chunk id
+            writeInt(output, 36 + rawData.length); // chunk size
+            writeString(output, "WAVE"); // format
+            writeString(output, "fmt "); // subchunk 1 id
+            writeInt(output, 16); // subchunk 1 size
+            writeShort(output, (short) 1); // audio format (1 = PCM)
+            writeShort(output, (short) 2); // number of channels
+            writeInt(output, 44100); // sample rate
+            writeInt(output, mSampleRate *2); // byte rate
+            writeShort(output, (short) 2); // block align
+            writeShort(output, (short) 16); // bits per sample
+            writeString(output, "data"); // subchunk 2 id
+            writeInt(output, rawData.length); // subchunk 2 size
+            // Audio data (conversion big endian -> little endian)
+            short[] shorts = new short[rawData.length / 2];
+            ByteBuffer.wrap(rawData).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
+            ByteBuffer bytes = ByteBuffer.allocate(shorts.length * 2);
+            for (short s : shorts) {
+                bytes.putShort(s);
+            }
+
+            output.write(fullyReadFileToBytes(rawFile));
+        } finally {
+            if (output != null) {
+                output.close();
+            }
+        }
+    }
+    byte[] fullyReadFileToBytes(File f) throws IOException {
+        int size = (int) f.length();
+        byte bytes[] = new byte[size];
+        byte tmpBuff[] = new byte[size];
+        FileInputStream fis= new FileInputStream(f);
+        try {
+
+            int read = fis.read(bytes, 0, size);
+            if (read < size) {
+                int remain = size - read;
+                while (remain > 0) {
+                    read = fis.read(tmpBuff, 0, remain);
+                    System.arraycopy(tmpBuff, 0, bytes, size - remain, read);
+                    remain -= read;
+                }
+            }
+        }  catch (IOException e){
+            throw e;
+        } finally {
+            fis.close();
+        }
+
+        return bytes;
+    }
+    private void writeInt(final DataOutputStream output, final int value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+        output.write(value >> 16);
+        output.write(value >> 24);
+    }
+
+    private void writeShort(final DataOutputStream output, final short value) throws IOException {
+        output.write(value >> 0);
+        output.write(value >> 8);
+    }
+
+    private void writeString(final DataOutputStream output, final String value) throws IOException {
+        for (int i = 0; i < value.length(); i++) {
+            output.write(value.charAt(i));
+        }
     }
     public int uploadFile(String sourceFileUri, String id) {
 
@@ -277,7 +524,7 @@ public class Controller_Voice extends Controller {
 
                             // messageText.setText(msg);
                             //Toast.makeText(RecActivity.this, "File Upload Complete.",
-                                //    Toast.LENGTH_SHORT).show();
+                            //    Toast.LENGTH_SHORT).show();
                         }
                     });
                 }
---
+++ CacheMission/app/src/main/java/com/example/hwshin/cachemission/DataStructure/Option/Option.java
@@ -0,0 +1,10 @@
+package com.example.hwshin.cachemission.DataStructure.Option;
+
+import com.example.hwshin.cachemission.DataStructure.TaskView.TaskView;
+
+public abstract class Option {
+    public int OptionID;
+    private int taskID;
+    public TaskView mtaskview;
+
+}
