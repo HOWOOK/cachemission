10c1900996305e3cb21bafa7f9da48b3d90d8837
HOWOOK <37730885+HOWOOK@users.noreply.github.com>
2018-08-28 21:52:49 +0900

    Revert "Revert "desgin0828""
    This reverts commit 25cf799a000b0347c17b2eabf4a0d377ff89c9ab.

---
+++ CacheMission/app/src/main/java/com/example/hwshin/cachemission/Activity/RegionActivity.java
@@ -0,0 +1,125 @@
+package com.example.hwshin.cachemission.Activity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.hwshin.cachemission.R;
+
+public class RegionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_region);
+
+        ImageView iv1 = findViewById(R.id.region1);
+        ImageView iv2 = findViewById(R.id.region2);
+        ImageView iv3 = findViewById(R.id.region3);
+        ImageView iv4 = findViewById(R.id.region4);
+        ImageView iv5 = findViewById(R.id.region5);
+        ImageView iv6 = findViewById(R.id.region6);
+        ImageView iv7 = findViewById(R.id.region7);
+        ImageView iv8 = findViewById(R.id.region8);
+
+        iv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "충북";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "경북";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "전북";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "충남";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "경남";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "전남";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "강원";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+        iv8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TaskActivity.region_dialect = "제주";
+                SharedPreferences explain = getSharedPreferences("region", MODE_PRIVATE);
+                SharedPreferences.Editor editor = explain.edit();
+                editor.putString("region", TaskActivity.region_dialect);
+                editor.commit();
+                finish();
+            }
+        });
+
+
+    }
+}
