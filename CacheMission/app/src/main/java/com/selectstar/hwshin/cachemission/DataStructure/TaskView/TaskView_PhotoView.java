7b87c71c4d86372bf9e6f1cf76a350fc2c33c113
HOWOOK <37730885+HOWOOK@users.noreply.github.com>
2018-09-18 19:29:50 +0900

    0918
    2dbox 수정중

--- CacheMission/app/src/main/java/com/selectstar/hwshin/cachemission/DataStructure/TaskView/TaskView_PhotoView.java
+++ CacheMission/app/src/main/java/com/selectstar/hwshin/cachemission/DataStructure/TaskView/TaskView_PhotoView.java
@@ -4,7 +4,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
-import android.support.v4.content.ContextCompat;
+import android.support.constraint.ConstraintSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -22,13 +22,18 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 public class TaskView_PhotoView extends TaskView {
-    private PhotoView myView;
+    private PhotoView photoView;
     private float[][] answerCoordination;
     private String[] array1, array2, array3;
+    private ConstraintLayout photoVIewCL;
 
     public TaskView_PhotoView()
     {
         taskViewID = R.layout.taskview_photoview;
+    }
+
+    public PhotoView getPhotoView() {
+        return this.photoView;
     }
 
     @Override
@@ -43,21 +48,18 @@ public class TaskView_PhotoView extends TaskView {
 
     public void setContent(String content)
     {
+        photoVIewCL = parentActivity.findViewById(R.id.photoViewCL);
         //box 좌표 구해서 저장
         array1 = content.split("&");
         array2 = array1[1].split("\\(");
         answerCoordination = new float[array2.length][4];
-        for(int i =0; i < array2.length; i++){
-            array3 = array2[i].split(",");
-            answerCoordination[i][0] = (float) Float.parseFloat(array3[1]);
-            answerCoordination[i][1] = (float) Float.parseFloat(array3[2]);
-            answerCoordination[i][2] = (float) Float.parseFloat(array3[3]);
-            answerCoordination[i][3] = (float) Float.parseFloat(array3[4]);
-        }
-        myView = parentActivity.findViewById(R.id.srcview);
-        myView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        coordinationParsing(array2);
 
-               Glide.with(parentActivity)
+
+
+        photoView = parentActivity.findViewById(R.id.srcview);
+        photoView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+        Glide.with(parentActivity)
                 .load(Uri.parse(parentActivity.getString(R.string.mainurl)+"/media/"+ array1[0]))
                 .listener(new RequestListener<Drawable>() {
                     @Override
@@ -67,16 +69,18 @@ public class TaskView_PhotoView extends TaskView {
 
                     @Override
                     public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-
+                        System.out.println("나여기들어옴1");
+                        coordinationChange(answerCoordination);
+                        drawAnswer(answerCoordination);
                         return true;
                     }
                 })
                 .thumbnail(0.1f)
-                .into(myView)
+                .into(photoView)
         ;
 
 
-        myView.setOnTouchListener(new View.OnTouchListener() {
+        photoView.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
 
@@ -87,8 +91,51 @@ public class TaskView_PhotoView extends TaskView {
             }
         });
 
+    }
+
+    private void drawAnswer(float[][] answerCoordination) {
+        ConstraintLayout answerCL = new ConstraintLayout(parentActivity);
+        answerCL.setLayoutParams(new ConstraintLayout.LayoutParams(0,0));
+        ConstraintLayout mAnswerCL = (ConstraintLayout) answerCL;
+        photoVIewCL.addView(mAnswerCL);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(mAnswerCL);
+        constraintSet.connect(answerCL.getId(), ConstraintSet.TOP, photoVIewCL.getId(), ConstraintSet.TOP);
+        constraintSet.connect(answerCL.getId(), ConstraintSet.START, photoVIewCL.getId(),ConstraintSet.START);
+        constraintSet.applyTo(mAnswerCL);
+
+        ImageView answer = new ImageView(parentActivity);
+        answer.setLayoutParams(new ConstraintLayout.LayoutParams(30, 30));
+        answer.setBackgroundResource(R.drawable.box_2dbox);
+        answerCL.addView(answer);
 
 
+    }
 
+    //퍼센트 좌표를 절대 좌표로 변경해준다.
+    private void coordinationChange(float[][] answerCoordination) {
+        float photoWidth, photoHeight;
+        photoWidth = photoView.getDisplayRect().right - photoView.getDisplayRect().left;
+        photoHeight = photoView.getDisplayRect().bottom - photoView.getDisplayRect().top;
+        for (int i = 0; i<answerCoordination.length; i++){
+            System.out.println("나여기들어옴3");
+            answerCoordination[i][0] = answerCoordination[i][0] * photoWidth; //left
+            answerCoordination[i][1] = answerCoordination[i][1] * photoHeight; //top
+            answerCoordination[i][2] = answerCoordination[i][2] * photoWidth; //right
+            answerCoordination[i][3] = answerCoordination[i][3] * photoHeight; //bottom
+        }
+    }
+
+    //String 형태로 서버에서 넘어온 좌표를 float[][] 형식으로 바꾸어 answerCoordination에 넣어줌
+    private void coordinationParsing(String[] array) {
+        for(int i =1; i < array2.length; i++){
+            System.out.println("나여기들어옴2");
+            array3 = array2[i].split(",");
+            answerCoordination[i-1][0] = (float) Float.parseFloat(array3[1]);
+            answerCoordination[i-1][1] = (float) Float.parseFloat(array3[2]);
+            answerCoordination[i-1][2] = (float) Float.parseFloat(array3[3]);
+            answerCoordination[i-1][3] = (float) Float.parseFloat(array3[4]);
+        }
     }
 }
